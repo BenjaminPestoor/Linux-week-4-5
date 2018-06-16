bab1e8a4dba6fb6916157e7fc808d90d6a099a84
BenjaminPestoor <benjamin_pestoor@hotmail.com>
2018-06-16 13:26:22 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 apt install salt-master -y
 apt install salt-ssh -y
+
+
+#==============================================
+#CLIENT SERVER SALT
+#==============================================
+mkdir /srv/salt/
+mkdir /srv/salt/states/
+mkdir /srv/salt/states/base/
+mkdir /srv/salt/states/base/snmpd/
+mkdir /srv/salt/states/base/snmp
+mkdir /srv/salt/states/base/rsyslog
+mkdir /srv/salt/states/base/php
+mkdir /srv/salt/states/base/mysql-client
+mkdir /srv/salt/states/base/apache2
