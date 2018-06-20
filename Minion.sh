e6c31d874931991354d44642de6be039f43a96a7
BenjaminPestoor <benjamin_pestoor@hotmail.com>
2018-06-20 08:53:53 +0200

    Update Minion.sh

--- Minion.sh
+++ Minion.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+#==============================================
+#MINION SALT INSTALL
+#==============================================
+
+apt install salt-minion -y
+
+#==============================================
+#MINION SALT SETTINGS
+#==============================================
+  #Downloading salt-minion config file
+wget http://10.1.1.6/salt-master/salt-minion/minion.conf -O /etc/salt/minion.d/minion.conf
+  
+  #restarting salt-services
+service salt-minion restart
