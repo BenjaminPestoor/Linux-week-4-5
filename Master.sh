6a9f3a4ab3613b19a664da53d021507b5fc67f46
BenjaminPestoor <benjamin_pestoor@hotmail.com>
2018-06-28 13:04:53 +0200

    Update Master.sh

--- Master.sh
+++ Master.sh
@@ -23,6 +23,8 @@ mkdir -p /srv/salt/states/base/rsyslogmaster
 mkdir -p /srv/salt/states/base/rsyslogminion
 mkdir -p /srv/salt/states/base/snmp
 mkdir -p /srv/salt/states/base/snmpd
+mkdir -p /srv/salt/states/base/unzip
+mkdir -p /srv/salt/states/base/wordpress
 
   #Salt Pillars directories
 mkdir -p /srv/salt/pillars/
@@ -96,15 +98,12 @@ service apache2 restart
 #MASTER SALT STATE FILES FOR MINION
 #==============================================
   #Downloading Apache2 init file
-mkdir -p /srv/salt/states/base/apache2/
 wget http://10.1.1.6/salt-master/apache2/init.sls -O /srv/salt/states/base/apache2/init.sls
 
   #Downloading mysql-client init file
-mkdir -p /srv/salt/states/base/mysql-client/
 wget http://10.1.1.6/salt-master/mysql-client/init.sls -O /srv/salt/states/base/mysql-client/init.sls
 
   #Downloading php init file
-mkdir -p /srv/salt/states/base/php/
 wget http://10.1.1.6/salt-master/php/init.sls -O /srv/salt/states/base/php/init.sls
 
   #Downloading rsyslog init file and config
@@ -114,11 +113,9 @@ wget http://10.1.1.6/salt-master/rsyslog.minion/remote.conf -O /srv/salt/states/
 wget http://10.1.1.6/salt-master/rsyslog.master/master.conf -O /srv/salt/states/base/rsyslogmaster/master.conf
 
   #Downloading snmp init file
-mkdir -p /srv/salt/states/base/snmp/
 wget http://10.1.1.6/salt-master/snmp/init.sls -O /srv/salt/states/base/snmp/init.sls
 
   #Downloading snmpd init file and config
-mkdir -p /srv/salt/states/base/snmpd/
 wget http://10.1.1.6/salt-master/snmpd/init.sls -O /srv/salt/states/base/snmpd/init.sls
 wget http://10.1.1.6/salt-master/snmpd/snmpd.conf -O /srv/salt/states/base/snmpd/snmpd.conf
 
@@ -140,10 +137,18 @@ sleep 10
 echo "sleeping done"
 
   #read top.sls and start salt
-salt '*' state.apply snmp
-salt '*' state.apply snmpd
+salt 'Ubuntu-1710-Salty-Minion' state.apply snmp
+salt 'Ubuntu-1710-Salty-Minion' state.apply snmpd
 salt 'Ubuntu-1710-Master-Salt-final' state.apply rsyslogmaster
 salt 'Ubuntu-1710-Salty-Minion' state.apply rsyslogminion
-salt '*' state.apply php
-salt '*' state.apply apache2
+salt 'Ubuntu-1710-Salty-Minion' state.apply php
+salt 'Ubuntu-1710-Salty-Minion' state.apply apache2
 salt 'Ubuntu-1710-Salty-Minion' state.apply mysql-client
+
+
+#==============================================
+# WORDPRESS
+#==============================================
+wget http://40.115.23.218/salt-master/wordpress/init.slss -O /srv/salt/states/base/wordpress/init.sls
+wget http://40.115.23.218/salt-master/wordpress/script.sh -O /srv/salt/states/base/wordpress/script.sh
+salt 'Ubuntu-1710-Salty-Minion' state.apply wordpress
